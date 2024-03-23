b7970aed724ac6fb657983eeee9309422045d820
Bearcin46 <imsweety33@gmail.com>
2024-03-23 19:34:40 +0530

    feat: name changed

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -7,11 +7,11 @@ const Header: React.FC = () => {
   const { count, cartOpen } = useGlobalContent();
   return (
     <div className="flex items-center justify-between px-6 md:px-10 py-3 bg-[#723d46]">
-      <div className="flex gap-2 text-white items-center">
+      <div className="flex gap-1 text-white items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="2em"
-          height="2em"
+          height="1.5em"
           viewBox="0 0 256 256"
         >
           <path
@@ -19,7 +19,7 @@ const Header: React.FC = () => {
             d="M224 104h-8.37a88 88 0 0 0-175.26 0H32a8 8 0 0 0-8 8a104.35 104.35 0 0 0 56 92.28V208a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16v-3.72A104.35 104.35 0 0 0 232 112a8 8 0 0 0-8-8m-50.52-47.77q2.75 2.25 5.27 4.75a87.92 87.92 0 0 0-49.15 43h-29.5A72.26 72.26 0 0 1 168 56c1.83 0 3.66.09 5.48.23M148.12 104a71.84 71.84 0 0 1 41.27-29.57A71.45 71.45 0 0 1 199.54 104ZM128 40a71.87 71.87 0 0 1 19 2.57A88.36 88.36 0 0 0 83.33 104H56.46A72.08 72.08 0 0 1 128 40"
           ></path>
         </svg>
-        <h1 className="text-xl font-serif font-semibold">Anbu Unavagam</h1>
+        <h1 className="text-2xl font-serif font-semibold">Nadine's Hotel</h1>
       </div>
       <div className="flex">
         <div onClick={cartOpen} className="cursor-pointer">
